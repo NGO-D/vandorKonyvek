1223c95e5c1e9719c9f716364512ccdeae7895c5
NGO-D <csonkakisandrea@gmail.com>
2020-09-01 16:40:21 +0200

    init

--- ex-vandorKonyvek-projekt/app.js
+++ ex-vandorKonyvek-projekt/app.js
@@ -9,6 +9,18 @@ const usersRouter = require('./routes/users');
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Header",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  );
+  next();
+});
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
