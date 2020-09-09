72712b8aca713c868643724fb7ef07bc2a3281b8
NGO-D <csonkakisandrea@gmail.com>
2020-09-09 19:32:48 +0200

    angular material installed

--- vandorKonyvek-project/src/app/app.module.ts
+++ vandorKonyvek-project/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MockComponent } from './mock/mock.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { MockComponent } from './mock/mock.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
