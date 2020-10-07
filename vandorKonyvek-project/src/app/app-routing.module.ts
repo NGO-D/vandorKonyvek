d726c060d4f9e931f89f00718da36b00ea4e57c2
NGO-D <csonkakisandrea@gmail.com>
2020-10-07 18:19:51 +0200

    components created, material added

--- vandorKonyvek-project/src/app/app-routing.module.ts
+++ vandorKonyvek-project/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MockComponent } from './mock/mock.component';
-
+import { LoginComponent } from './components/admin-component/auth/login/login.component';
+import { SignUpComponent } from './components/admin-component/auth/sign-up/sign-up.component';
+import { AdminLandingComponent} from './components/admin-component/admin-landing/admin-landing.component';
 
 const routes: Routes = [
- { path: 'mock', component: MockComponent }
+ { path: 'mock', component: MockComponent },
+ { path: 'admin/login', component: LoginComponent},
+ { path: 'admin/signup', component: SignUpComponent},
+ { path: 'admin/home', component: AdminLandingComponent}
 ];
 
 @NgModule({
--- vandorKonyvek-project/src/app/app.module.ts
+++ vandorKonyvek-project/src/app/app.module.ts
@@ -8,18 +8,30 @@ import { AppComponent } from './app.component';
 import { MockComponent } from './mock/mock.component';
 import { MockService } from './services/mock.service';
 import { AdminListsComponent } from './components/admin-component/admin-lists/admin-lists.component';
+import { NavComponent } from './components/admin-component/nav/nav.component';
+import { AdminEditComponent } from './components/admin-component/admin-component/admin-edit/admin-edit.component';
+import { MaterialModule } from './material.module';
+import { SignUpComponent } from './components/admin-component/auth/sign-up/sign-up.component';
+import { LoginComponent } from './components/admin-component/auth/login/login.component';
+import { AdminLandingComponent } from './components/admin-component/admin-landing/admin-landing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MockComponent,
-    AdminListsComponent
+    AdminListsComponent,
+    NavComponent,
+    AdminEditComponent,
+    SignUpComponent,
+    LoginComponent,
+    AdminLandingComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [MockService],
   bootstrap: [AppComponent]
