36bb77c1240860fd2ee14842f03f52f5dcbf4ea3
NGO-D <csonkakisandrea@gmail.com>
2020-10-14 15:32:57 +0200

    table in book-list is created

--- vandorKonyvek-projekt/src/app/components/admin-component/books/book-list/book-list.component.ts
+++ vandorKonyvek-projekt/src/app/components/admin-component/books/book-list/book-list.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+// import { Subscription } from 'rxjs/Subscription';
+
+
+import { BooksService } from '../../../../services/books.service';
+import { Books } from '../../../../models/books.model';
 
 @Component({
   selector: 'app-book-list',
@@ -6,10 +12,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./book-list.component.css']
 })
 export class BookListComponent implements OnInit {
+  books: Books[];
 
-  constructor() { }
+  constructor(private booksService: BooksService,
+              private router: Router,
+              private activatedRoute: ActivatedRoute) 
+              { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    console.log("Books megy :D");
+    this.booksService.get().subscribe(books => {
+      this.books = books;
+    });
   }
+
+//   nemműködik, az egyéni oldal után jó lesz
+ onDeleteOneBook(id) {
+   console.log('lúúúúúúúúúúúúúúúúúúúúúúzer')
+ this.booksService.delete(id);
+}
 
 }
