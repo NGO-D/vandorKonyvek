5d2d970935ba75a6e01619fc456c6f483eb8db9a
NGO-D <csonkakisandrea@gmail.com>
2021-02-16 14:18:54 +0100

    signup error handling duplicate usernames

--- nest-vandor-konyvek/src/auth/user.repository.ts
+++ nest-vandor-konyvek/src/auth/user.repository.ts
@@ -1,6 +1,7 @@
 import { EntityRepository, Repository } from "typeorm";
 import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
 import { User } from "./user.entity";
+import { ConflictException, InternalServerErrorException } from '@nestjs/common';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
@@ -10,6 +11,17 @@ export class UserRepository extends Repository<User> {
         const user = new User();
         user.username = username;
         user.password = password;
+
+        try {
+            await user.save();
+        } catch (error) {
+            if (error.code === '23505') { // duplicate username. 
+                // az error.codokat mindig '' közé kell tenni
+                throw new ConflictException('Duplicate username');
+            } else {
+                throw new InternalServerErrorException();
+            }
+        }
         await user.save();
     }
 }
