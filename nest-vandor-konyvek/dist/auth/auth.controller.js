d8fb21b4ce73167ae389839cf9fe18389921791f
NGO-D <csonkakisandrea@gmail.com>
2021-03-03 14:04:33 +0100

    get-user decorator created

--- nest-vandor-konyvek/dist/auth/auth.controller.js
+++ nest-vandor-konyvek/dist/auth/auth.controller.js
@@ -17,6 +17,8 @@ const common_1 = require("@nestjs/common");
 const auth_service_1 = require("./auth.service");
 const auth_credentials_dto_1 = require("./dto/auth-credentials.dto");
 const passport_1 = require("@nestjs/passport");
+const get_user_decorator_1 = require("./get-user.decorator");
+const user_entity_1 = require("./user.entity");
 let AuthController = class AuthController {
     constructor(authService) {
         this.authService = authService;
@@ -27,8 +29,8 @@ let AuthController = class AuthController {
     signIn(authCredentialsDto) {
         return this.authService.signIn(authCredentialsDto);
     }
-    test(req) {
-        console.log(req);
+    test(user) {
+        console.log(user);
     }
 };
 __decorate([
@@ -48,9 +50,9 @@ __decorate([
 __decorate([
     common_1.Post('/test'),
     common_1.UseGuards(passport_1.AuthGuard()),
-    __param(0, common_1.Req()),
+    __param(0, get_user_decorator_1.GetUser()),
     __metadata("design:type", Function),
-    __metadata("design:paramtypes", [Object]),
+    __metadata("design:paramtypes", [user_entity_1.User]),
     __metadata("design:returntype", void 0)
 ], AuthController.prototype, "test", null);
 AuthController = __decorate([
--- nest-vandor-konyvek/src/auth/auth.controller.ts
+++ nest-vandor-konyvek/src/auth/auth.controller.ts
@@ -2,6 +2,8 @@ import { Body, Controller, Post, ValidationPipe, UseGuards, Req } from '@nestjs/
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from './get-user.decorator';
+import { User } from './user.entity';
 
 @Controller('auth')
 export class AuthController {
@@ -18,10 +20,6 @@ export class AuthController {
     signIn(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto): Promise<{accessToken: string}> {
         return this.authService.signIn(authCredentialsDto);
     }
-
-    @Post('/test')
-    @UseGuards(AuthGuard())
-    test(@Req() req) {
-        console.log(req);
-    }
+    
 }
+
